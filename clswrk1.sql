1ce56e388ba79428495198283d8e292f12a371a6
ananth2344 <krishnananantha093@gmail.com>
2025-10-13 11:58:45 +0530

    commit

---
+++ clswrk1.sql
@@ -0,0 +1,16 @@
+INSERT INTO students (id, name, age, department, grade) VALUES
+(1, 'Ananya Sharma', 19, 'Computer Science', 85),
+(2, 'Rahul Menon', 22, 'Mechanical', 78),
+(3, 'Sneha Nair', 21, 'Physics', 90),
+(4, 'Arjun Reddy', 23, 'Electronics', 67);
+SELECT * FROM students
+WHERE age > 20;
+SELECT * FROM students
+WHERE department IN ('Computer Science', 'Physics');
+SELECT * FROM students
+WHERE grade = 90;
+SELECT * FROM students
+WHERE grade BETWEEN 70 AND 90;
+
+
+
