6ccfb353c1958b472f9d8b6e01680ef5fa81a4ad
ananth2344 <krishnananantha093@gmail.com>
2025-11-03 11:16:39 +0530

    commit4

--- clswrk9.sql
+++ clswrk9.sql
@@ -1,4 +1,6 @@
 CREATE TABLE students (student_id INT PRIMARY KEY,name VARCHAR(100),email VARCHAR(100) UNIQUE);
 CREATE TABLE courses (course_id INT PRIMARY KEY,course_name VARCHAR(100));
-CREATE TABLE enrollments (student_id INT,course_id INT,PRIMARY KEY (student_id, course_id),FOREIGN KEY (student_id) REFERENCES students(student_id),FOREIGN KEY (course_id) REFERENCES courses(course_id));
+CREATE TABLE enrollments (student_id INT,course_id INT,PRIMARY KEY (student_id, course_id),FOREIGN KEY (student_id) 
+REFERENCES students(student_id),FOREIGN KEY (course_id) REFERENCES courses(course_id));
+
 
